fdc3e3ef74636ac80d68e6427b4d2fba1ff508b4
congyuwang <leonwang998@gmail.com>
2023-06-23 18:45:53 +0800

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use clap::Parser;
+use std::path::PathBuf;
+use tokenizers::models::wordpiece::{WordPiece, WordPieceTrainer};
+use tokenizers::normalizers::{strip::Strip, unicode::NFC, utils::Sequence};
+use tokenizers::pre_tokenizers::byte_level::ByteLevel;
+use tokenizers::{AddedToken, TokenizerBuilder};
+
+#[derive(Parser)]
+struct Cli {
+    /// save path
+    #[clap(short, long)]
+    out: PathBuf,
+
+    #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
+    txt: Vec<String>,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let mut trainer = WordPieceTrainer::builder()
+        .vocab_size(50000)
+        .show_progress(true)
+        .special_tokens(vec![
+            AddedToken::from(String::from("[PAD]"), true),
+            AddedToken::from(String::from("[UNK]"), true),
+            AddedToken::from(String::from("[CLS]"), true),
+            AddedToken::from(String::from("[SEP]"), true),
+            AddedToken::from(String::from("[MASK]"), true),
+        ])
+        .build();
+
+    let mut tokenizer = TokenizerBuilder::new()
+        .with_model(WordPiece::default())
+        .with_normalizer(Some(Sequence::new(vec![
+            Strip::new(true, true).into(),
+            NFC.into(),
+        ])))
+        .with_pre_tokenizer(Some(ByteLevel::default()))
+        .with_post_processor(Some(ByteLevel::default()))
+        .with_decoder(Some(ByteLevel::default()))
+        .build()
+        .expect("Failed to build tokenizer");
+
+    tokenizer
+        .train_from_files(&mut trainer, cli.txt)
+        .expect("Failed to train")
+        .save(cli.out, false)
+        .expect("Failed to save");
+}
