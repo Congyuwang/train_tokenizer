1f03edbdf744d0e441c2dcd074ca5c6727b5a47a
congyuwang <leonwang998@gmail.com>
2023-06-23 22:57:49 +0800

    read from db

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use rocksdb::{Cache, DBRawIteratorWithThreadMode, Env, Options, ReadOptions, DB};
 use std::path::PathBuf;
 use tokenizers::models::wordpiece::{WordPiece, WordPieceTrainer};
 use tokenizers::normalizers::{strip::Strip, unicode::NFC, utils::Sequence};
@@ -15,9 +16,51 @@ struct Cli {
     #[clap(short, long)]
     out: PathBuf,
 
+    #[clap(short, long)]
+    db: Option<PathBuf>,
+
     /// list of input txt text files
     #[clap(short, long, value_parser, num_args = 1.., value_delimiter = ' ')]
     txt: Vec<String>,
+}
+
+struct KeyIter<'a> {
+    inner: DBRawIteratorWithThreadMode<'a, DB>,
+    key_count_estimate: usize,
+    first: bool,
+}
+
+impl<'a> KeyIter<'a> {
+    fn new(db: &'a DB, key_count_estimate: usize) -> Self {
+        let mut read_opt = ReadOptions::default();
+        read_opt.set_async_io(true);
+        let mut inner = db.raw_iterator_opt(read_opt);
+        inner.seek_to_first();
+        Self {
+            inner,
+            key_count_estimate,
+            first: true,
+        }
+    }
+}
+
+impl<'a> Iterator for KeyIter<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<String> {
+        if !self.first {
+            self.inner.next();
+        } else {
+            self.first = false;
+        }
+        self.inner
+            .key()
+            .map(|b| String::from_utf8_lossy(b).to_string())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (0, Some(self.key_count_estimate))
+    }
 }
 
 fn main() {
@@ -47,9 +90,28 @@ fn main() {
         .build()
         .expect("Failed to build tokenizer");
 
-    tokenizer
-        .train_from_files(&mut trainer, cli.txt)
-        .expect("Failed to train")
-        .save(cli.out, false)
-        .expect("Failed to save");
+    if let Some(db) = cli.db {
+        let env = Env::new().expect("failed to create env");
+        let cache = Cache::new_lru_cache(1024 * 1024 * 8);
+        let (opts, cfs) =
+            Options::load_latest(&db, env, true, cache).expect("failed to load cache");
+        let db =
+            DB::open_cf_descriptors_read_only(&opts, db, cfs, false).expect("Failed to open db");
+        let estimate_key = db
+            .property_int_value("rocksdb.estimate-num-keys")
+            .expect("failed to get estimate key num")
+            .unwrap() as usize;
+        let key_iter = KeyIter::new(&db, estimate_key);
+        tokenizer
+            .train(&mut trainer, key_iter)
+            .expect("Failed to train")
+            .save(cli.out, false)
+            .expect("Failed to save");
+    } else {
+        tokenizer
+            .train_from_files(&mut trainer, cli.txt)
+            .expect("Failed to train")
+            .save(cli.out, false)
+            .expect("Failed to save");
+    }
 }
